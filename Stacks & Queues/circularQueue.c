dad0ac56616a1382abf09aa84ab0d8176b8a3bb8
Karthikeyan <karthikeyan.36947@gmail.com>
2024-06-20 14:37:34 +0530

    Update circularQueue.c

--- Stacks & Queues/circularQueue.c
+++ Stacks & Queues/circularQueue.c
@@ -1,115 +1,97 @@
-#include<stdio.h>
-#include<stdlib.h>  
-#include<conio.h>   
+// Circular Queue implementation in C
 
-#define max 3
+#include <stdio.h>
 
-int q[10], front=0, rear=-1;
+#define SIZE 5
 
-// Function prototypes
-void insert();
-void delet();
-void display();
+int items[SIZE];
+int front = -1, rear = -1;
 
-void main()
-{
-    int ch;
-
-    clrscr();
-    printf("\nCircular Queue operations\n");
-    printf("1.insert\n2.delete\n3.display\n4.exit\n");
-
-    while(1)
-    {
-        printf("Enter your choice:");
-        scanf("%d", &ch);
-
-        switch(ch)
-        {
-            case 1:
-                insert();
-                break;
-            case 2:
-                delet();
-                break;
-            case 3:
-                display();
-                break;
-            case 4:
-                exit(0);
-            default:
-                printf("Invalid option\n");
-        }
-    }
+// Check if the queue is full
+int isFull() {
+  if ((front == rear + 1) || (front == 0 && rear == SIZE - 1)) return 1;
+  return 0;
 }
 
-void insert()
-{
-    int x;
-    if ((front == 0 && rear == max - 1) || (front > 0 && rear == front - 1))
-        printf("Queue is overflow\n");
-    else
-    {
-        printf("Enter element to be inserted:");
-        scanf("%d", &x);
-
-        if (rear == max - 1 && front > 0)
-            rear = 0;
-        else
-            rear = (rear + 1) % max;
-
-        q[rear] = x;
-    }
+// Check if the queue is empty
+int isEmpty() {
+  if (front == -1) return 1;
+  return 0;
 }
 
-void delet()
-{
-    int a;
-    if (front == 0 && rear == -1)
-    {
-        printf("Queue is underflow\n");
-        getch();
-        exit(0);
-    }
-
-    a = q[front];
-    if (front == rear)
-    {
-        front = 0;
-        rear = -1;
-    }
-    else
-        front = (front + 1) % max;
-
-    printf("Deleted element is: %d\n", a);
+// Adding an element
+void enQueue(int element) {
+  if (isFull())
+    printf("\n Queue is full!! \n");
+  else {
+    if (front == -1) front = 0;
+    rear = (rear + 1) % SIZE;
+    items[rear] = element;
+    printf("\n Inserted -> %d", element);
+  }
 }
 
-void display()
-{
-    int i;
-
-    if (front == 0 && rear == -1)
-    {
-        printf("Queue is underflow\n");
-        getch();
-        exit(0);
+// Removing an element
+int deQueue() {
+  int element;
+  if (isEmpty()) {
+    printf("\n Queue is empty !! \n");
+    return (-1);
+  } else {
+    element = items[front];
+    if (front == rear) {
+      front = -1;
+      rear = -1;
+    } 
+    // Q has only one element, so we reset the 
+    // queue after dequeing it. ?
+    else {
+      front = (front + 1) % SIZE;
     }
+    printf("\n Deleted element -> %d \n", element);
+    return (element);
+  }
+}
 
-    printf("Queue elements are:");
-    if (front <= rear)
-    {
-        for (i = front; i <= rear; i++)
-            printf(" %d", q[i]);
+// Display the queue
+void display() {
+  int i;
+  if (isEmpty())
+    printf(" \n Empty Queue\n");
+  else {
+    printf("\n Front -> %d ", front);
+    printf("\n Items -> ");
+    for (i = front; i != rear; i = (i + 1) % SIZE) {
+      printf("%d ", items[i]);
     }
-    else
-    {
-        for (i = front; i < max; i++)
-            printf(" %d", q[i]);
+    printf("%d ", items[i]);
+    printf("\n Rear -> %d \n", rear);
+  }
+}
 
-        for (i = 0; i <= rear; i++)
-            printf(" %d", q[i]);
-    }
+int main() {
+  // Fails because front = -1
+  deQueue();
 
-    printf("\n");
-    getch();
+  enQueue(1);
+  enQueue(2);
+  enQueue(3);
+  enQueue(4);
+  enQueue(5);
+
+  // Fails to enqueue because front == 0 && rear == SIZE - 1
+  enQueue(6);
+
+  display();
+  deQueue();
+
+  display();
+
+  enQueue(7);
+  display();
+
+  // Fails to enqueue because front == rear + 1
+  enQueue(8);
+
+  return 0;
 }
