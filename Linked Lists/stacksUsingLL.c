c83ddedff4fbc2fc1e5613ccef07f45325bee943
Karthikeyan <karthikeyan.36947@gmail.com>
2024-06-20 19:10:47 +0530

    Create stacksUsingLL.c

---
+++ Linked Lists/stacksUsingLL.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* top = NULL;
+
+void push(int item) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    newNode->data = item;
+    newNode->next = top;
+    top = newNode;
+    printf("%d is successfully pushed\n", item);
+}
+
+void pop() {
+    if (top == NULL) {
+        printf("Stack is empty\n");
+        return;
+    }
+    struct Node* temp = top;
+    printf("%d is successfully popped\n", temp->data);
+    top = top->next;
+    free(temp);
+}
+
+void display() {
+    struct Node* temp = top;
+    if (temp == NULL) {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Status of stack\n");
+    while (temp != NULL) {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int option, item;
+    while (1) {
+        printf("\nStack operations\n");
+        printf("1.Push\n2.Pop\n3.Display\n4.Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &option);
+        switch (option) {
+        case 1:
+            printf("Enter the item to be pushed: ");
+            scanf("%d", &item);
+            push(item);
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
+        default:
+            printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}
